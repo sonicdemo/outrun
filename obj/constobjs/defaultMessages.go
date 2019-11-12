13f142ba9b61a0a3765a1c20cd416a74cca40631
Mtbcooler <40797305+Mtbcooler@users.noreply.github.com>
2019-11-12 20:19:10 +0100

    Changed Unix time stamps and fixed import issue

--- obj/constobjs/defaultMessages.go
+++ obj/constobjs/defaultMessages.go
@@ -1,15 +1,13 @@
 package constobjs
 
 import (
-    "os/exec"
-
     "github.com/Mtbcooler/outrun/obj"
 )
 
 var DefaultInformations = []obj.Information{
-    obj.NewInformation(531, 1, 1465808400, 1466413199, "3__10500013_14"),
+    obj.NewInformation(531, 1, 1465808400, 1609405749, "3__10500013_14"),
     //obj.NewInformation(6001070, 1, 1464336180, 1580608922, "2_[ffff00]'You feel it too, don't you?'[ffffff]\nWelcome to [ff0000]OUTRUN[ffffff], a custom Sonic Runners server!\n\n(Last local Git update: "+getLastGitCommitDate()+")\r\n_10600001_0"),
-    obj.NewInformation(6001070, 1, 1464336180, 1580608922, "2_[ffff00]Welcome to the updated Runners Revival. Now powered by Outrun.\r\n_10600001_0"),
+    obj.NewInformation(6001070, 1, 1464336180, 1609405749, "2_[ffff00]Welcome to the updated Runners Revival. Now powered by Outrun.\r\n_10600001_0"),
     //obj.NewInformation(1000230, 3, 1465981200, 1466413199, "1__90000001_1"),
     //obj.NewInformation(1000157, 600, 1448614800, 1609459199, "1__90000002_1"),
 }
