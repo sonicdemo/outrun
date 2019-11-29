08bc15e1affbcc950b0a0635275db59e41727599
Mtbcooler <40797305+Mtbcooler@users.noreply.github.com>
2019-11-29 12:44:18 +0100

    Update itemCosts.go

--- consts/itemCosts.go
+++ consts/itemCosts.go
@@ -4,15 +4,15 @@ import "github.com/Mtbcooler/outrun/enums"
 
 var ItemPrices = map[string]int64{
     enums.ItemIDStrInvincible: 3000,
-    enums.ItemIDStrBarrier:    1000,
+    enums.ItemIDStrBarrier:    4000,
     enums.ItemIDStrMagnet:     3000,
     enums.ItemIDStrTrampoline: 2000,
-    enums.ItemIDStrCombo:      3000,
-    enums.ItemIDStrLaser:      5000,
-    enums.ItemIDStrDrill:      4000,
-    enums.ItemIDStrAsteroid:   5000,
+    enums.ItemIDStrCombo:      5000,
+    enums.ItemIDStrLaser:      2000,
+    enums.ItemIDStrDrill:      2000,
+    enums.ItemIDStrAsteroid:   1000,
 
-    enums.ItemIDStrBoostScore:      6000,
-    enums.ItemIDStrBoostTrampoline: 1000,
-    enums.ItemIDStrBoostSubChara:   4000,
+    enums.ItemIDStrBoostScore:      5000,
+    enums.ItemIDStrBoostTrampoline: 2000,
+    enums.ItemIDStrBoostSubChara:   3000,
 }
