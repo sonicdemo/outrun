dfdcbb1b090858965f0bc6325eacb97bc0fba929
FairPlay137 <danielws424@gmail.com>
2019-12-14 01:29:11 -0800

    Fix the fix so it doesn't crash when using boosters

--- logic/gameplay/requiredItemPayment.go
+++ logic/gameplay/requiredItemPayment.go
@@ -1,29 +1,33 @@
 package gameplay
 
 import (
-    "github.com/Mtbcooler/outrun/netobj"
-    "github.com/Mtbcooler/outrun/obj"
-    "github.com/Mtbcooler/outrun/obj/constobjs"
+	"github.com/Mtbcooler/outrun/netobj"
+	"github.com/Mtbcooler/outrun/obj"
+	"github.com/Mtbcooler/outrun/obj/constobjs"
 )
 
 func findItem(id string) obj.ConsumedItem {
-    var result obj.ConsumedItem
-    for _, citem := range constobjs.DefaultCostList {
-        if citem.ID == id {
-            result = citem
-            break
-        }
-    }
-    return result
+	var result obj.ConsumedItem
+	for _, citem := range constobjs.DefaultCostList {
+		if citem.ID == id {
+			result = citem
+			break
+		}
+	}
+	return result
 }
 
 func GetRequiredItemPayment(items []string, player netobj.Player) int64 {
 	totalRingPayment := int64(0)
 	for _, itemID := range items {
 		citem := findItem(itemID)
-		index := player.IndexOfItem(itemID)
-		if player.PlayerState.Items[index].Amount < 1 {
+		if itemID[:2] == "11" { // boosts, not items
 			totalRingPayment += citem.Item.Amount
+		} else {
+			index := player.IndexOfItem(itemID)
+			if player.PlayerState.Items[index].Amount < 1 {
+				totalRingPayment += citem.Item.Amount
+			}
 		}
 	}
 	return totalRingPayment
