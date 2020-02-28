8cc6fd1ed1383e1f9584d8fc5bde8a3b6f990780
FairPlay137 <danielws424@gmail.com>
2020-02-27 18:46:43 -0800

    (UNTESTED) Beginnings of shutdown handler

--- muxhandlers/muxobj/objMuxHandler.go
+++ muxhandlers/muxobj/objMuxHandler.go
@@ -13,36 +13,47 @@ import (
 	"github.com/Mtbcooler/outrun/helper"
 )
 
+var shuttingDown = false
+
 func Handle(f func(*helper.Helper), logExecutionTime bool) func(w http.ResponseWriter, r *http.Request) {
 	funcnameSplit := strings.Split(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), "/")
 	funcname := funcnameSplit[len(funcnameSplit)-1]
 	funcnameSplit = strings.Split(funcname, ".") // just get function name
 	funcname = funcnameSplit[len(funcnameSplit)-1]
-	if logExecutionTime {
+	if shuttingDown {
+		// prevent any requests from going through in order to stop any possible edits to the database while Outrun is shutting down
 		return func(w http.ResponseWriter, r *http.Request) {
-			startTime := time.Now()
-			help := helper.MakeHelper(funcname, w, r)
-			if config.CFile.LogAllRequests {
-				nano := time.Now().UnixNano()
-				nanoStr := strconv.Itoa(int(nano))
-				filename := help.Request.RequestURI + "--" + nanoStr
-				filename = strings.ReplaceAll(filename, ".", "-")
-				filename = strings.ReplaceAll(filename, "/", "-") + ".txt"
-				filepath := "logging/all_requests/" + filename
-				help.Out("DEBUG: Saving request to " + filepath)
-				err := ioutil.WriteFile(filepath, help.GetGameRequest(), 0644)
-				if err != nil {
-					help.Out("DEBUG ERROR: Unable to write file '" + filepath + "'")
-				}
-			}
-			f(help)
-			endTime := time.Now()
-			timeDiff := endTime.Sub(startTime)
-			help.Out("Done executing in " + timeDiff.String())
+			w.WriteHeader(http.StatusServiceUnavailable)
+			w.Write([]byte("Service Unavailable"))
 		}
-	}
-	return func(w http.ResponseWriter, r *http.Request) {
-		help := helper.MakeHelper(funcname, w, r)
-		f(help)
+	} else {
+		if logExecutionTime {
+			return func(w http.ResponseWriter, r *http.Request) {
+				startTime := time.Now()
+				help := helper.MakeHelper(funcname, w, r)
+				if config.CFile.LogAllRequests {
+					nano := time.Now().UnixNano()
+					nanoStr := strconv.Itoa(int(nano))
+					filename := help.Request.RequestURI + "--" + nanoStr
+					filename = strings.ReplaceAll(filename, ".", "-")
+					filename = strings.ReplaceAll(filename, "/", "-") + ".txt"
+					filepath := "logging/all_requests/" + filename
+					help.Out("DEBUG: Saving request to " + filepath)
+					err := ioutil.WriteFile(filepath, help.GetGameRequest(), 0644)
+					if err != nil {
+						help.Out("DEBUG ERROR: Unable to write file '" + filepath + "'")
+					}
+				}
+				f(help)
+				endTime := time.Now()
+				timeDiff := endTime.Sub(startTime)
+				help.Out("Done executing in " + timeDiff.String())
+			}
+		} else {
+			return func(w http.ResponseWriter, r *http.Request) {
+				help := helper.MakeHelper(funcname, w, r)
+				f(help)
+			}
+		}
 	}
 }
