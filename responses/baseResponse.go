819e9ba5fbbd1ae8bb8dc5643987588390a3fa18
FairPlay137 <danielws424@gmail.com>
2020-03-27 22:03:03 -0700

    Preparation for...something...

--- responses/baseResponse.go
+++ responses/baseResponse.go
@@ -2,26 +2,29 @@ package responses
 
 import (
 	"github.com/Mtbcooler/outrun/consts"
+	"github.com/Mtbcooler/outrun/meta"
 	"github.com/Mtbcooler/outrun/responses/responseobjs"
 )
 
 type BaseResponse struct {
 	responseobjs.BaseInfo
-	AssetsVersion     string `json:"assets_version"`
+	AssetsVersion     string `json:"assets_version"` // doesn't necessarily have to be a number
 	ClientDataVersion string `json:"client_data_version"`
 	DataVersion       string `json:"data_version"`
 	InfoVersion       string `json:"info_version"`
 	Version           string `json:"version"`
+	OutrunVersion     string `json:"ORN_version"`
 }
 
 func NewBaseResponse(base responseobjs.BaseInfo) BaseResponse {
 	return BaseResponse{
 		base,
-		"049",
+		"april",
 		"2.0.3",
 		"15",
 		"017",
 		"2.0.3",
+		meta.Version,
 	}
 }
 
@@ -33,5 +36,6 @@ func NewBaseResponseV(base responseobjs.BaseInfo, gameVersion string) BaseRespon
 		"15",
 		"017",
 		gameVersion,
+		meta.Version,
 	}
 }
